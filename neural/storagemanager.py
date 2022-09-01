cf281d36c81202bd59f279df27e67cb760b29a19
Andrea Lizzit <andrea.lizzit@gmail.com>
2022-09-01 11:36:15 +0200

    change default directories

--- neural/storagemanager.py
+++ neural/storagemanager.py
@@ -1,20 +1,26 @@
 from datetime import datetime
 from pathlib import Path
 from collections import namedtuple
+from itertools import cache
 
 dataset_pair = namedtuple("dataset_pair", ["train", "test"])
 
 class StorageManager():
 	""" Consistent management of directories and locations for neural models """
 
-	def __init__(self, models_dir = "train", data_dir = "train", tensorboard_logdir="runs"):
-		self.models_dir = models_dir
-		self.data_dir = data_dir
+	def __init__(self,
+					models_dir = "out/models",
+					data_dir = "out/datasets",
+					tensorboard_logdir="out/runs",
+					version=1):
+		self.models_dir = Path(models_dir)
+		self.data_dir = Path(data_dir)
 		self.tbdir = tensorboard_logdir
-	
+		self.version = version
+
 	def sessions(self):
 		""" Returns a list of all sessions in the models directory """
-		return list(Path(self.models_dir).glob("v1_*"))
+		return list(self.models_dir.glob(f"v{self.version}_*"))
 	
 	def models(self, session):
 		""" Returns a list of all models in the session """
@@ -26,12 +32,15 @@ class StorageManager():
 	
 	def datasets(self):
 		""" Returns a list of all datasets in the data directory """
-		# this is a little weak: if the *_train and *_test are not always in pairs,
-		# it will mess up all the following pairings
-		return [dataset_pair(train, test) for train, test in zip(Path(self.data_dir).glob("*_train"), Path(self.data_dir).glob("*_test"))]
+		return [dataset_pair(dir / "train", dir/"test") for dir in Path(self.data_dir).glob(f"v{self.version}_dataset_*")]
 	
 	def tensorboard_logdir(self):
 		return self.tbdir
-	
+
+	@cache	
 	def new_session(self):
-		return Path(self.models_dir) / "v1_{}".format(datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+		return self.models_dir / "v{}_{}".format(self.version, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+	
+	@cache
+	def new_dataset(self):
+		return self.data_dir / "v{}_dataset_{}".format(self.version, datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
